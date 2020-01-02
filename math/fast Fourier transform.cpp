95bdabb85e40e2be8744908400fb586d178cadc2
0214sh7 <50052448+0214sh7@users.noreply.github.com>
2020-01-02 22:00:27 +0900

    Update fast Fourier transform.cpp

--- math/fast Fourier transform.cpp
+++ math/fast Fourier transform.cpp
@@ -2,10 +2,10 @@ class fast_Fourier_transform{
     private:
     const double PI=3.14159265358979; 
     
-    vector<complex<double>> DFT(vector<complex<double>> A){
+    std::vector<std::complex<double>> DFT(std::vector<std::complex<double>> A){
         int N=A.size();
         if(N==1)return A;
-        vector<complex<double>> A0(N/2),A1(N/2),iA0,iA1,iA(N);
+        std::vector<std::complex<double>> A0(N/2),A1(N/2),iA0,iA1,iA(N);
         for(int i=0;i<N;i++){
             if(i%2==0){
                 A0[i/2]=A[i];
@@ -17,31 +17,31 @@ class fast_Fourier_transform{
         iA1=DFT(A1);
         
         for(int i=0;i<N;i++){
-            complex<double> ith_zeta = complex<double>(cos(2*PI*i/N),sin(2*PI*i/N));
+            std::complex<double> ith_zeta = std::complex<double>(cos(2*PI*i/N),sin(2*PI*i/N));
             iA[i]=(iA0[i%(N/2)]+ ith_zeta*iA1[i%(N/2)]);
         }
         return iA;
     }
     
-    vector<complex<double>> iDFT(vector<complex<double>> iA){
+    std::vector<std::complex<double>> iDFT(std::vector<std::complex<double>> iA){
         int N=iA.size();
-        vector<complex<double>> A,dA,rA;
+        std::vector<std::complex<double>> A,dA,rA;
         dA=DFT(iA);
         for(int i=0;i<N;i++){
             rA.push_back(dA[(N-i)%N]);
-            A.push_back(rA[i]/complex<double>(N,0));
+            A.push_back(rA[i]/std::complex<double>(N,0));
         }
         return A;
     }
     
     public:
-    vector<int> convolution(vector<int> A,vector<int> B){
+    std::vector<long long> convolution(std::vector<long long> A,std::vector<long long> B){
         int deg = A.size() + B.size() -1;
-        int N=1;
+        long long N=1;
         while(N<deg){N<<=1;}
         A.resize(N);B.resize(N);
-        vector<complex<double>> dC(N),iC(N),dA,iA,dB,iB;
-        vector<int> C(N);
+        std::vector<std::complex<double>> dC(N),iC(N),dA,iA,dB,iB;
+        std::vector<long long> C(N);
         for(int i=0;i<A.size();i++){
             dA.push_back(A[i]);
         }
@@ -54,7 +54,7 @@ class fast_Fourier_transform{
         }
         dC=iDFT(iC);
         for(int i=0;i<dC.size();i++){
-            C[i]=(int)(0.1+dC[i].real());
+            C[i]=(0.1+dC[i].real());
         }
         return C;
     }
