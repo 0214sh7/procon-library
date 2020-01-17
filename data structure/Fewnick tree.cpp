7e62791d63ffedfd07c4fcf84ff91942c3e7ff61
0214sh7 <50052448+0214sh7@users.noreply.github.com>
2020-01-18 00:06:56 +0900

    Create Fewnick tree.cpp

---
+++ data structure/Fewnick tree.cpp
@@ -0,0 +1,37 @@
+class Fewnick_tree{
+    private:
+    std::vector<long long> BIT;
+    
+    public:
+    
+    void add(int a,long long w){
+        for(int x=a;x<BIT.size();x|=(x+1)){
+            BIT[x]+=w;
+        }
+    }
+    
+    void init(int n){
+        BIT.clear();
+        for(int i=0;i<n;i++){
+            BIT.push_back(0);
+        }
+    }
+    
+    long long sum(int a){
+        long long r=0;
+        for(int x=a-1;x>=0;x=(x&(x+1))-1){
+            r+=BIT[x];
+        }
+        return r;
+    }
+    
+    long long inversion(std::vector<long long> V){
+        long long r=0;
+        init(V.size());
+        for(int i=0;i<V.size();i++){
+            add(V[i],1);
+            r+=i-sum(V[i]);
+        }
+        return r;
+    }
+};
